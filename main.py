9be109c4ceded92c0d7a48c5967c343d25c79846
Shevch376 <symaexplay5430@gmail.com>
2026-04-03 10:18:16 +0700

    feat: сохранение пароля

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import random
+import string
+
 class Password:
     def __init__(self, value):
         self.value = value
 
+import random
+import string
+
+def generate_password(length=8):
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
 
 passwords = []
-
-print("Password generator app")
