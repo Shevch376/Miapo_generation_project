2be368ea99f0b8023e1541a85dd20acfe31d751f
Shevch376 <symaexplay5430@gmail.com>
2026-04-03 10:33:36 +0700

    feat: delete password

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def generate_password(length=8):
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
 
+def delete_password(index):
+    passwords.pop(index)
 
 passwords = []
 
